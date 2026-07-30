b06ba893fb4b35a27a58d53c9e6998b540c39b15
Govind <govind12771277@gmail.com>
2026-07-30 23:17:42 +0530

    for check purpose

--- cohort/mixedQuestion/sumofdigits.cpp
+++ cohort/mixedQuestion/sumofdigits.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string govind(string nothing) {
+  cout << "kya haal chal hai " << endl;
+  return nothing;
+}
+
 int main() {
-    string n;
-    cin >> n;
+  string n;
+  cin >> n;
 
-    int sum = 0;
+  int sum = 0;
 
-    for (char c : n) {
-        sum += c - '0';
-    }
+  for (char c : n) {
+    sum += c - '0';
+  }
 
-    cout << sum << '\n';
+  cout << sum << '\n';
 
-    return 0;
+  return 0;
 }
